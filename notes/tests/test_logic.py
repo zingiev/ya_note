bffa869a9f1c3ae09425b5698953f287b152b511
Yasin <yasinzingiev@mail.ru>
2024-09-11 09:49:51 +0300

    the end

--- notes/tests/test_logic.py
+++ notes/tests/test_logic.py
@@ -56,17 +56,44 @@ class TestNotesCreator(TestCase):
 class TestNoteEditDelete(TestCase):
     NOTES_TITLE = 'Новая заметка'
     NOTES_TEXT = 'Текст заметки'
+    NEW_NOTES_TEXT = 'Новый текст'
 
     @classmethod
     def setUpTestData(cls) -> None:
-        cls.author = User.objects.get(username='Кот рыжий')
+        cls.author = User.objects.create(username='Кот рыжий')
         cls.author_client = Client()
         cls.author_client.force_login(cls.author)
         cls.reader = User.objects.create(username='Читатель')
-        cls.resder_client = Client()
-        cls.resder_client.force_login(cls.reader)
+        cls.reader_client = Client()
+        cls.reader_client.force_login(cls.reader)
+        cls.note = Note.objects.create(
+            title=cls.NOTES_TITLE, text=cls.NOTES_TEXT, author=cls.author)
         cls.edit_url = reverse('notes:edit', args=(cls.note.slug,))
         cls.delete_url = reverse('notes:delete', args=(cls.note.slug,))
         cls.form_data = {
-            
+            'title': cls.NOTES_TITLE,
+            'text': cls.NEW_NOTES_TEXT,
+            'author': cls.author
         }
+
+    def test_author_can_delete_note(self):
+        self.author_client.delete(self.delete_url)
+        notes_count = Note.objects.count()
+        self.assertEqual(notes_count, 0)
+
+    def test_user_cant_delete_note_of_another_user(self):
+        response = self.reader_client.delete(self.delete_url)
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+        notes_count = Note.objects.count()
+        self.assertEqual(notes_count, 1)
+
+    def test_author_can_edit_note(self):
+        self.author_client.post(self.edit_url, data=self.form_data)
+        self.note.refresh_from_db()
+        self.assertEqual(self.note.text, self.NEW_NOTES_TEXT)
+
+    def test_user_cant_edit_note_of_another_user(self):
+        response = self.reader_client.post(self.edit_url, data=self.form_data)
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+        self.note.refresh_from_db()
+        self.assertEqual(self.note.text, self.NOTES_TEXT)
