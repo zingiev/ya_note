c71547ba55187782cd74c2dbbc2f2dbed5c7fed0
zingiev <m@zingiev.ru>
2024-09-11 06:50:33 +0300

    последний метод остался

--- notes/tests/test_logic.py
+++ notes/tests/test_logic.py
@@ -40,3 +40,33 @@ class TestNotesCreator(TestCase):
         self.assertEqual(note.title, self.NOTES_TITLE)
         self.assertEqual(note.text, self.NOTES_TEXT)
         self.assertEqual(note.author, self.user)
+
+    def test_cant_create_two_to_notes(self):
+        for i in range(2):
+            self.auth_client.post(self.url, data=self.form_data)
+        notes_count = Note.objects.count()
+        self.assertEqual(notes_count, 1)
+
+    def test_filling_slug(self):
+        self.auth_client.post(self.url, data=self.form_data)
+        note = Note.objects.get()
+        self.assertIsNotNone(note.slug)
+
+
+class TestNoteEditDelete(TestCase):
+    NOTES_TITLE = 'Новая заметка'
+    NOTES_TEXT = 'Текст заметки'
+
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.author = User.objects.get(username='Кот рыжий')
+        cls.author_client = Client()
+        cls.author_client.force_login(cls.author)
+        cls.reader = User.objects.create(username='Читатель')
+        cls.resder_client = Client()
+        cls.resder_client.force_login(cls.reader)
+        cls.edit_url = reverse('notes:edit', args=(cls.note.slug,))
+        cls.delete_url = reverse('notes:delete', args=(cls.note.slug,))
+        cls.form_data = {
+            
+        }
